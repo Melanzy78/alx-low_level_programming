35264d16fb6e76fe525474e1498fe8e74b216622
yanoyvonne <yanoevon@gmail.com>
2022-09-08 11:17:07 -0700

    its hard

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  *
  * Return: 0
  */
-int main (void)
+int main(void)
 {
 	int i;
 	long int l;
@@ -15,9 +15,8 @@ int main (void)
 
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(l));
-        printf("Size of a long long int: %ld byte(s)\n", sizeof(ll));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(l))
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(ll));
 	printf("Size of a float: %ld byte(s)\n", sizeof(f));
-        return (0);
-
-}	
+        return (0)
+}
