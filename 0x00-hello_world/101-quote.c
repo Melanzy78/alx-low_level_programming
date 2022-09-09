943e00a2e1dc848700615b64a811e73026bf4921
yanoyvonne <yanoevon@gmail.com>
2022-09-09 12:43:44 -0700

    task7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints "and that piece of art is useful"
  *
  * Return: 1
  */
 
 int main(void)
 {
-fwrite(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 return (1);
 }
