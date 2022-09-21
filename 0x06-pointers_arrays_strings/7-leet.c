0c3ab47d2ec043c2007edc3599deb787fe75f83f
yanoyvonne <yanoevon@gmail.com>
2022-09-21 10:14:56 -0700

    task7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,25 +2,27 @@
 
 /**
  * leet - encode a string into 1337
- * @s: string
- * Return: encoded string `s`
+ * @c: string pointer
+ * Return: char pointer
  */
 
-char *leet(char *s)
+char *leet(char *c)
 {
-int i;
-for (i = 0; s[i] != '\0'; i++)
+int i = 0, j;
+char a[] = {'a', 'e', 'o', 't', 'l'}, b[] = {'A', 'E', 'O', 'T', 'L'},
+leet[] = {'4', '3', '0', '7', '1'};
+
+while (c[i] != '\0')
 {
-while (s[i] == 'a' || s[i] == 'A')
-	s[i] = '4';
-while (s[i] == 'e' || s[i] == 'E')
-	s[i] = '3';
-while (s[i] == 'o' || s[i] == 'O')
-	s[i] = '0';
-while (s[i] == 't' || s[i] == 'T')
-	s[i] = '7';
-while (s[i] == 'l' || s[i] == 'L')
-	s[i] = '1';
+j = 0;
+while (j < 5)
+{
+if (c[i] == a[j] || c[i] == b[j])
+c[i] = leet[j];
+j++;
 }
-return (s);
+i++;
+
+}
+return (c);
 }
