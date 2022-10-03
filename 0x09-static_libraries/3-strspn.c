85a70766f3486fde0289eff4937a0c02476b9585
yanoyvonne <yanoevon@gmail.com>
2022-10-03 04:28:56 -0700

    task0

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -1,33 +1,31 @@
 #include "main.h"
-
 /**
- * _strspn - gets the length of a prefix substring
- * @s: string to evaluate
- * @accept: string containing the list of characters to match in s
- * Return: the number of bytes in the initial segment
- * of s which consist only of bytes from accept
- */
+*_strspn - search the number of bytes in the initial
+* segment of s which consist only of bytes from accept
+*@s:segment targeted
+*@accept:reference bytes container
+*
+*Return:returns the number of bytes in the initial
+* segment of s which consist only of bytes from accept
+*/
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j, f, flag;
+	unsigned int bytes = 0;
+	int i;
 
-	f = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
-{
-	flag = 0;
-	for (j = 0; accept[j] != '\0'; j++)
+	while (*s)
 	{
-		if (s[i] == accept[j])
+		for (i = 0; accept[i]; i++)
 		{
-			f++;
-			flag = 1;
+			if (accept[i] == *s)
+			{
+				bytes++;
+				break;
+			}
+			else if ((accept[i + 1]) == '\0')
+				return (bytes);
 		}
+		s++;
 	}
-	if (flag == 0)
-	{
-		return (f);
-	}
-}
-return (0);
+	return (bytes);
 }
--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
- * _strcpy - Copy paste string
- * @dest: destination
- * @src: source
- * Return: destination
- */
+* _strcpy - Copy paste string
+*@dest: destination
+*@src: source
+*Return: dest
+*/
 char *_strcpy(char *dest, char *src)
 {
 int inc = 0;
