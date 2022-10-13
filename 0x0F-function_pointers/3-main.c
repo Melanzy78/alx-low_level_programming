15350cad7c6648ca4de670ec4b4e1900a9af3ec1
yanoyvonne <yanoevon@gmail.com>
2022-10-13 12:59:48 -0700

    task3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -21,7 +21,7 @@ exit(98);
 }
 a = atoi(argv[1]);
 b = atoi(argv[3]);
-f = get_op_func(argv[2];
+f = get_op_func(argv[2]);
 if (f == NULL)
 {
 printf("Error\n");
@@ -30,4 +30,5 @@ exit(99);
 c = f(a, b);
 printf("%d\n", c);
 return (0);
+
 }
