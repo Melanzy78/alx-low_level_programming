f66f3d041f9a74fb24d9f390861fca24647e1993
yanoyvonne <yanoevon@gmail.com>
2022-10-13 07:34:03 -0700

    task0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,10 +5,10 @@
  *
  *  @name: character string
  *   @f: function pointer
- *   Return: nothing
+ *   Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if{f}
-	f{name};
+if (name && f)
+f(name);
 }
